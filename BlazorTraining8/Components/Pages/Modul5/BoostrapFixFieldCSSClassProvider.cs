2ab19e551873eeabc0a380919a9d9574fcadaf7f
Hannes Preishuber <hannesp@ppedv.de>
2024-04-26 11:06:00 +0200

    CSS Fix für Boostrap validierung

---
+++ BlazorTraining8/Components/Pages/Modul5/BoostrapFixFieldCSSClassProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorTraining8.Components.Pages.Modul5
+{
+    public class BoostrapFixFieldCSSClassProvider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+           bool IsInValid= editContext.GetValidationMessages(fieldIdentifier).Any();
+            return IsInValid ? "is-invalid" : "is-valid";
+            
+        }
+    }
+}
