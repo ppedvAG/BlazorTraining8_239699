7a4ee329a6586c2e1ab2373335dbd7e531a5ebe8
Hannes Preishuber <hannesp@ppedv.de>
2024-04-26 15:45:20 +0200

    JavaScript Simple

---
+++ BlazorTraining8/Components/Pages/modul8/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorTraining8.Components.Pages.modul8
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
